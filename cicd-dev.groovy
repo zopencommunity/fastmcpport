bc4b2cf95fc6498d8f5c5005cd920e77a15cfab6
Igor Todorovski <itodorov@ca.ibm.com>
2026-08-10 17:04:21 -0400

    Rename: drop python- prefix from repo name and internal references

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/python-fastmcpport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/fastmcpport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/python-fastmcpport.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/fastmcpport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'FastMCP framework for building Model Context Protocol (MCP) servers in Python'),
       string(name: 'BUILD_LINE', value: 'DEV'),
       booleanParam(name: 'PUBLISH_PYTHON_WHEEL', value: true)
